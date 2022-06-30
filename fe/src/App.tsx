499c562f584f77fd0eccd2859852fcd44273fd8e
Anatolii Kabanov <4akabanov@gmail.com>
2022-06-30 21:52:54 +0300

    Updated dependencies to make solana libraries work. Config overrides to properly build. Backend commands to build/deploy.

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,17 +1,31 @@
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import React from 'react';
 import './App.css';
+import '@solana/wallet-adapter-react-ui/styles.css';
 import MainView from './views/main-view';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 
 interface AppProps {
 
 }
 
+const network = 'http://127.0.0.1:8899';
+const supportedWallets = [ new PhantomWalletAdapter() ];
+
 const App: React.FC<AppProps> = () => {
-  return (
-    <div className="App">
-      <MainView/>
-    </div>
-  );
-}
+    return (
+        <ConnectionProvider endpoint={network}>
+            <WalletProvider wallets={supportedWallets} autoConnect>
+                <WalletModalProvider>
+                    <div className='App'>
+                        <MainView network={network} />
+                    </div>
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
 
 export default App;
+
--- fe/src/views/main-view.tsx
+++ fe/src/views/main-view.tsx
@@ -1,22 +1,31 @@
-import { AnchorProvider, Wallet } from '@project-serum/anchor';
-import { Commitment, Connection } from '@solana/web3.js';
 import React from 'react';
+import { AnchorProvider, Idl, Program } from '@project-serum/anchor';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { Commitment, Connection, Keypair, PublicKey, SystemProgram } from '@solana/web3.js';
+import idl from '../be.json';
 
 const opts = {
     preflightCommitment: 'processed' as Commitment,
 };
+/* create an account  */
+const baseAccount = Keypair.generate();
+const programId = new PublicKey(idl.metadata.address);
 
-export const MainView: React.FC = () => {
+interface MainViewProps {
+    network: string;
+}
 
+export const MainView: React.FC<MainViewProps> = ({ network }) => {
+    console.log('baseAccount', baseAccount)
+    const wallet = useWallet();
     const getProvider = async () => {
         /* create the provider and return it to the caller */
         /* network set to local network for now */
-        const network = 'http://127.0.0.1:8899';
         const connection = new Connection(network, opts.preflightCommitment);
-
         const provider = new AnchorProvider(
             connection,
-            {} as Wallet,
+            wallet as any,
             opts,
         );
         return provider;
@@ -24,11 +33,36 @@ export const MainView: React.FC = () => {
 
     const createAccount = async () => {
         const provider = await getProvider();
-        console.log('provider:', provider)
+        console.log('provider:', provider);
+        /* create the program interface combining the idl, program ID, and provider */
+        const program = new Program(idl as Idl, programId, provider);
+        try {
+            /* interact with the program via rpc */
+            await program.rpc.create({
+                accounts: {
+                    baseAccount: baseAccount.publicKey,
+                    user: wallet.publicKey!,
+                    systemProgram: SystemProgram.programId,
+                },
+                signers: [baseAccount]
+            });
+
+            const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+            console.log('account: ', account);
+      
+        } catch (err) {
+            console.log("Transaction error: ", err);
+        }
     }
-    return <div className='main-view'>
-        <button onClick={createAccount}>Create Account</button>
-    </div>;
+
+    return !wallet.connected ? (
+        <WalletMultiButton />
+    ) : (
+        <div className='main-view'>
+            <button onClick={createAccount}>Create Account</button>
+        </div>
+    );
+       
 };
 
 export default MainView;
